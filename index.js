b64fc4790a26313fadf51a72273caae7c9a31ee0
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-05-24 01:06:55 -0700

    added ascii art logo

--- index.js
+++ index.js
@@ -3,5 +3,22 @@ const {runInquirer, commandQuestion} = require('./js.api/inquirer');
 const init = () => {
 	runInquirer(commandQuestion);
 };
+//asciiart
+const logo = require('asciiart-logo');
+
+console.log(
+    logo({
+        name: 'Welcome To The employee data base',
+        font: 'Small Slant',
+        lineChars: 12,
+        padding: 2,
+        margin: 3,
+        borderColor: 'grey',
+        logoColor: 'bold-cyan',
+        textColor: 'cyan',
+    })
+    
+    .render()
+);
 
 init();
